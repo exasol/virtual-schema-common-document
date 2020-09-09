593bd3746e5bcd9336620b1b9d58a58fe43afb90
Jakob Braun <jakob.braun@posteo.de>
2020-09-09 09:38:21 +0200

    updated edml documentation

--- tools/previewSchemaDoc.sh
+++ tools/previewSchemaDoc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-readonly root_dir='dynamodb-virtual-schema'
-readonly schema_path='./src/main/resources/v1.json'
+readonly root_dir='virtual-schema-common-document'
+readonly schema_path='./src/main/resources/schemas/edml-1.0.0.json'
 readonly doc_dir='/tmp/doc/'
 
 prepare() {
