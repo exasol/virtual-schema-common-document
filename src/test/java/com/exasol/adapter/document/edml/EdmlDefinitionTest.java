db8710c6f28157129fde68f35a7b4ffacc2b135e
Jakob Braun <jakob.braun@posteo.de>
2022-04-06 05:08:04 -0300

    added Javadoc + tests for equals

--- src/test/java/com/exasol/adapter/document/edml/EdmlDefinitionTest.java
+++ src/test/java/com/exasol/adapter/document/edml/EdmlDefinitionTest.java
@@ -4,7 +4,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
 
 class EdmlDefinitionTest {
 
@@ -19,5 +26,17 @@ class EdmlDefinitionTest {
                 () -> assertThat(edmlDefinition.getDestinationTable(), equalTo("myTable")),
                 () -> assertThat("254 is the default value", idField.getVarcharColumnSize(), equalTo(254))//
         );
+    }
+
+    public static Stream<Arguments> getEdmlClasses() {
+        return Stream.of(EdmlDefinition.class, Fields.class, ToBoolMapping.class, ToDecimalMapping.class,
+                ToDecimalMapping.class, ToDoubleMapping.class, ToJsonMapping.class, ToTableMapping.class,
+                ToTimestampMapping.class, ToVarcharMapping.class).map(Arguments::of);
+    }
+
+    @ParameterizedTest
+    @MethodSource({ "getEdmlClasses" })
+    void testEquals(final Class<?> forClass) {
+        EqualsVerifier.simple().forClass(forClass).verify();
     }
 }
