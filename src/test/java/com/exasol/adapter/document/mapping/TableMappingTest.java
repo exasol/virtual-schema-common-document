e290868a8d72cb81cc8afca7b72444533d1f9a11
Pieterjan Spoelders <pj.spoelders@gmail.com>
2022-07-07 10:54:43 +0200

    fixed formatting

--- src/test/java/com/exasol/adapter/document/mapping/TableMappingTest.java
+++ src/test/java/com/exasol/adapter/document/mapping/TableMappingTest.java
@@ -1,11 +1,11 @@
 package com.exasol.adapter.document.mapping;
 
-import static com.exasol.adapter.document.mapping.PropertyToColumnMappingBuilderQuickAccess.getColumnMappingExample;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-import com.exasol.adapter.document.documentpath.DocumentPathExpression;
 import org.junit.jupiter.api.Test;
+
+import com.exasol.adapter.document.documentpath.DocumentPathExpression;
 
 class TableMappingTest {
     @Test
@@ -14,10 +14,12 @@ class TableMappingTest {
         final TableMapping table = TableMapping.rootTableBuilder("", "", testValue).build();
         assertThat(table.getAdditionalConfiguration(), equalTo(testValue));
     }
+
     @Test
     void additionalPropertiesReturnedNestedTableBuilderTest() {
         final String testValue = "testAdditionalConfig";
-        final TableMapping table = TableMapping.nestedTableBuilder("", "",DocumentPathExpression.builder().build(), testValue).build();
+        final TableMapping table = TableMapping
+                .nestedTableBuilder("", "", DocumentPathExpression.builder().build(), testValue).build();
         assertThat(table.getAdditionalConfiguration(), equalTo(testValue));
     }
 }
